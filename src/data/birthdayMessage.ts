17497d08204a920c93801b6a92fd071bc12755bc
ZonateMeteor <123872054+ZonateMeteor@users.noreply.github.com>
2026-08-07 10:29:39 +0300

    Last Sync: 2026-08-07 10:29 (Mobile)

--- src/data/birthdayMessage.ts
+++ src/data/birthdayMessage.ts
@@ -7,12 +7,18 @@
 // Hediyenin ve mesajın gösterileceği kişinin ismi:
 export const recipientName: string = "Jinda";
 
-export const birthdayMessage: string = `Doğum günün kutlu olsun!
+export const birthdayMessage: string = `Doğum günün kutlu olsunnn
 
-Bugün senin günün. Sana özel bu küçük dünyayı hazırladım.
-Şiirler, itiraflar, anılar, müzikler ve oyunlar seni bekliyor.
+Bugün senin doğum günün. iyi ki doğdun iyi ki varsın 
+Hayatımda tanıdığım en mükemmel kız <3
+Sana özel bu küçük dünyayı hazırladım.
+Şiirler, anılar, müzikler ve oyunlar var biraz az bir şey.
+aslında sana hediye almak isterdim ama işte sen karşı çıkınca bende
+iyi olduğum bir alana başvurdum göreceğin siteyi yaklaşık 2 haftadır kodluyorum ve totalde 6000 satırdan fazla kod var biraz uğraştırıcı oldj 
+pek ilgi alanın değil ama umarım beğenirsin bir nebze olsun moralini düzeltir yüzünde bir tebessüm olmasına meden olursam ne mutlu bana.
+seni seviyorum.
+
 
 Sol üstteki üç çizgiye basarak menüyü açabilir,
 oradan her bölüme ulaşabilirsin.
-
-İyi eğlenceler.`;
+Nice mutlu yıllaraaaar.`;
